e5c4b663c540d2e1c2bd873b0e76c92cd1a70fc6
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-14 20:47:48 -0500

    v1.0.0 for `ESP32_S3 + LwIP ENC28J60`
    1. Initial coding to port [ESPAsync_WiFiManager](https://github.com/khoih-prog/ESPAsync_WiFiManager) to ESP32_S3 boards using `LwIP ENC28J60 Ethernet`.
    2. Use `allman astyle`

---
+++ src/AsyncESP32_SC_ENC_Manager.h
@@ -0,0 +1,33 @@
+/****************************************************************************************************************************
+  AsyncESP32_SC_ENC_Manager.h
+
+  For Ethernet shields using ESP32_SC_ENC (ESP32_S2/S3/C3 + LwIP ENC28J60)
+
+  AsyncESP32_SC_ENC_Manager is a library for the ESP32_S2/S3/C3 with Ethernet ENC28J60 to run Async Credential Manager
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Alan Steremberg     (https://github.com/alanswx/ESPAsyncWiFiManager)
+  4. Khoi Hoang          (https://github.com/khoih-prog/ESPAsync_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncESP32_SC_ENC_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     14/12/2022 Initial coding for ESP32_S3 + LwIP ENC28J60
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef AsyncESP32_SC_ENC_Manager_h
+#define AsyncESP32_SC_ENC_Manager_h
+
+#include <AsyncESP32_SC_ENC_Manager.hpp>              //https://github.com/khoih-prog/AsyncESP32_SC_ENC_Manager
+#include <AsyncESP32_SC_ENC_Manager_Impl.h>           //https://github.com/khoih-prog/AsyncESP32_SC_ENC_Manager
+
+#endif    // AsyncESP32_SC_ENC_Manager_h
+
